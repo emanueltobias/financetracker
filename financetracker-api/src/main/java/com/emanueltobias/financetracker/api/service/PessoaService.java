a4c5d613e8edb113fdaca48500ccabf1c0c9d032
Emanuel Tobias <emanuel_tobias_@hotmail.com>
2020-01-28 11:38:07 -0300

    Atualização Pessoa com PUT

---
+++ financetracker-api/src/main/java/com/emanueltobias/financetracker/api/service/PessoaService.java
@@ -0,0 +1,26 @@
+package com.emanueltobias.financetracker.api.service;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.emanueltobias.financetracker.api.model.Pessoa;
+import com.emanueltobias.financetracker.api.repository.PessoaRepository;
+
+@Service
+public class PessoaService {
+	
+	@Autowired
+	private PessoaRepository pessoaRepository;
+
+	public Pessoa atualizar(Long codigo, Pessoa pessoa) {
+		Pessoa pessoaSalva = pessoaRepository.findOne(codigo);
+		if (pessoaSalva == null) {
+			throw new EmptyResultDataAccessException(1);
+		}
+		
+		BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo");
+		return pessoaRepository.save(pessoaSalva);
+	}
+}
