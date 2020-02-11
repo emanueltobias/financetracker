d44b7771e7b18ac4a6b98e8690833aa2478e8de2
Emanuel Tobias <emanuel_tobias_@hotmail.com>
2020-02-10 21:12:20 -0300

    Adicinada permissões de acesso Usuario/ClienteAplicação

--- financetracker-api/src/main/java/com/emanueltobias/financetracker/api/config/ResourceServerConfig.java
+++ financetracker-api/src/main/java/com/emanueltobias/financetracker/api/config/ResourceServerConfig.java
@@ -1,8 +1,11 @@
 package com.emanueltobias.financetracker.api.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -12,10 +15,13 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.expression.OAuth2MethodSecurityExpressionHandler;
 
 @Configuration
 @EnableWebSecurity
-@EnableResourceServer class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	
 	@Autowired
 	private UserDetailsService userDetailsService;
@@ -40,8 +46,16 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Res
 		resources.stateless(true);
 	}
 
-	private PasswordEncoder passwordEncoder() {
+	@Bean
+	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+	
+	@Bean
+	public MethodSecurityExpressionHandler createExpresionExpressionHandler() {
+		return new OAuth2MethodSecurityExpressionHandler();
+	}
+	
+	
 
 }
