49601591bec8b8cb2e9478f71c65db799e3066c6
Emanuel Tobias <emanuel_tobias_@hotmail.com>
2020-02-18 21:13:27 -0300

    Adicionada primeira tabela de dados

--- financetracker-ui/src/app/app.component.ts
+++ financetracker-ui/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Emanuel';
+
+
+  lancamentos = [
+    { tipo: 'DESPESA', descricao: 'Teste1', dataVencimento: '30/06/2020',
+      dataPagamento: null, valor: 55.55, pessoa: 'Teste1' },
+      { tipo: 'RECEITA', descricao: 'Teste2', dataVencimento: '30/06/2020',
+      dataPagamento: null, valor: 444.44, pessoa: 'Teste2' },
+      { tipo: 'DESPESA', descricao: 'Teste3', dataVencimento: '30/09/2020',
+      dataPagamento: '21/09/2020', valor: 99.99, pessoa: 'Teste3' }
+    ];
 }
--- financetracker-ui/src/app/app.module.ts
+++ financetracker-ui/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,7 +18,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     InputTextModule,
-    ButtonModule
+    ButtonModule,
+    TableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
