d34d4c7848f31cb4c733b7b6c219c5646cdf263a
Emanuel Tobias <emanuel_tobias_@hotmail.com>
2020-02-20 23:58:53 -0300

    Adicionado menu intercambiável

--- financetracker-ui/src/app/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
+++ financetracker-ui/src/app/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -13,6 +13,10 @@ export class LancamentosPesquisaComponent  {
       dataPagamento: null, valor: 55.55, pessoa: 'Teste1' },
       { tipo: 'RECEITA', descricao: 'Teste2', dataVencimento: '30/06/2020',
       dataPagamento: null, valor: 444.44, pessoa: 'Teste2' },
+      { tipo: 'RECEITA', descricao: 'Teste2', dataVencimento: '30/06/2020',
+      dataPagamento: null, valor: 444.44, pessoa: 'Teste2' },
+      { tipo: 'DESPESA', descricao: 'Teste2', dataVencimento: '30/06/2020',
+      dataPagamento: null, valor: 444.44, pessoa: 'Teste2' },
       { tipo: 'DESPESA', descricao: 'Teste3', dataVencimento: '30/09/2020',
       dataPagamento: '21/09/2020', valor: 99.99, pessoa: 'Teste3' }
     ];
