11e1b602f58ecdbf8c58e41a260606a920613340
Emanuel Tobias <emanuel_tobias_@hotmail.com>
2020-02-27 18:44:35 -0300

    Criação dos modulos Feature, Shared e Core

---
+++ financetracker-ui/src/app/pessoas/pessoas.module.ts
@@ -0,0 +1,45 @@
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { InputMaskModule } from 'primeng/inputmask';
+import { DropdownModule } from 'primeng/dropdown';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { TooltipModule } from 'primeng/tooltip';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+
+import { SharedModule } from './../shared/shared.module';
+import { PessoasGridComponent } from './pessoas-grid/pessoas-grid.component';
+import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.component';
+import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+
+    InputTextModule,
+    ButtonModule,
+    TableModule,
+    TooltipModule,
+    InputTextareaModule,
+    SelectButtonModule,
+    DropdownModule,
+    InputMaskModule,
+
+    SharedModule,
+  ],
+  declarations: [
+    PessoasPesquisaComponent,
+    PessoaCadastroComponent,
+    PessoasGridComponent,
+  ],
+  exports: [
+    PessoasPesquisaComponent,
+    PessoaCadastroComponent,
+  ]
+})
+export class PessoasModule { }
