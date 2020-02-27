ab0243830302166b7a685aa805064f5205fec637
Emanuel Tobias <emanuel_tobias_@hotmail.com>
2020-02-26 23:51:58 -0300

    Formulário de cadastro pessoas

--- financetracker-ui/src/app/app.module.ts
+++ financetracker-ui/src/app/app.module.ts
@@ -20,6 +20,7 @@ import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos
 import { NavbarComponent } from './navbar/navbar.component';
 import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
 import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-cadastro.component';
+import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.component';
 
 
 
@@ -30,6 +31,7 @@ import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-ca
     NavbarComponent,
     PessoasPesquisaComponent,
     LancamentoCadastroComponent,
+    PessoaCadastroComponent,
   ],
   imports: [
     BrowserModule,
