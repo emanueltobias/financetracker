9ffebfa291eda7daef9cec47812822700c1bf2b2
Emanuel Tobias <emanuel_tobias_@hotmail.com>
2020-03-01 00:10:09 -0300

    Implementação filtros especificos de lançamentos

--- financetracker-ui/src/app/lancamentos/lancamento.service.ts
+++ financetracker-ui/src/app/lancamentos/lancamento.service.ts
@@ -1,7 +1,14 @@
 import { Observable } from 'rxjs';
-import { map } from "rxjs/operators";
-import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import * as moment from 'moment';
+
+export interface LancamentoFiltro {
+  descricao: string;
+  dataVencimentoInicio: Date;
+  dataVencimentoFim: Date;
+}
 
 @Injectable({
   providedIn: 'root'
@@ -12,11 +19,28 @@ export class LancamentoService {
 
   constructor(private http: HttpClient) { }
 
-  pesquisar(): Observable<any> {
+  pesquisar(filtro: LancamentoFiltro): Observable<any> {
 
     let headers: HttpHeaders = new HttpHeaders();
+    let params: HttpParams = new HttpParams();
+
     headers = headers.append('Authorization', 'basic YWRtaW5AZW1hbnVlbHRvYmlhcy5jb206YWRtaW4=');
-    return this.http.get(`${this.lancamentosUrl}?resumo`, { headers })
+
+    if (filtro.descricao) {
+      params = params.append('descricao', filtro.descricao);
+    }
+
+    if (filtro.dataVencimentoInicio) {
+      params = params.append('dataVencimentoDe',
+       moment(filtro.dataVencimentoInicio).format('YYYY-MM-DD'));
+    }
+
+    if (filtro.dataVencimentoFim) {
+      params = params.append('dataVencimentoAt',
+      moment(filtro.dataVencimentoFim).format('YYYY-MM-DD'));
+    }
+
+    return this.http.get(`${this.lancamentosUrl}?resumo`, { headers, params })
     .pipe(
       map(res => res['content'])
 );
--- financetracker-ui/src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
+++ financetracker-ui/src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -1,4 +1,4 @@
-import { LancamentoService } from './../lancamento.service';
+import { LancamentoService, LancamentoFiltro } from './../lancamento.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LancamentosPesquisaComponent implements OnInit  {
 
-
+  descricao: string;
+  dataVencimentoInicio: Date;
+  dataVencimentoFim: Date;
   lancamentos = [];
 
   constructor(private lancamentoService: LancamentoService) {}
@@ -18,7 +20,13 @@ export class LancamentosPesquisaComponent implements OnInit  {
   }
 
   pesquisar() {
-    this.lancamentoService.pesquisar()
+    const filtro: LancamentoFiltro = {
+      descricao: this.descricao,
+      dataVencimentoInicio: this.dataVencimentoInicio,
+      dataVencimentoFim: this.dataVencimentoFim
+    };
+
+    this.lancamentoService.pesquisar(filtro)
     .subscribe(lancamentos => this.lancamentos = lancamentos);
   }
 
